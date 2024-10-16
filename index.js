3fbbb842bd27b066a5e65d6145c45e55324cbbaa
janine pamintuan <jaja.ninnin@gmail.com>
2024-10-16 10:38:56 -0400

    sending data fetch

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Add your code here
+// const formData = {
+//     dogName: "Byron",
+//     dogBreed: "Poodle",
+//     }
+
+// const configurationObject = {
+//     method: "POST",
+//     headers: {
+//         "Content-Type": "application/json",
+//         "Accept": "application/json",
+//     },
+//     body: JSON.stringify(formData)
+// };
+
+// fetch("http://localhost:3000/dogs", configurationObject)
+//     .then((response) => response.json())
+//     .then((object) => console.log(object))
+//     .catch((error) => {
+//         alert("Bad things!");
+//         console.log(error.message)
+//     })
+
+function submitData(userName, userEmail) {
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify({
+            name: userName,
+            email: userEmail,
+        })
+    })
+    .then((response) => response.json())
+    .then((user) =>  document.body.append(user.id))
+    .catch((error) => document.body.append(error.message))
+}
+submitData("abc", "abc@gmail.com");
